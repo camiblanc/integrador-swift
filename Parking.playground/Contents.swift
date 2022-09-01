646424c82880a7363752674ddba145ccfceb4196
Camila Blanc Fick <camila.blanc@mercadolibre.com>
2022-09-01 19:43:25 -0300

    update models

--- Parking.playground/Contents.swift
+++ Parking.playground/Contents.swift
@@ -1,57 +1,61 @@
 import Foundation
 
-struct Parking {
-  var vehicles: Set<Vehicle>
+protocol Parkable {
+    var plate: String { get }
+    var type: VehicleType { get }
+    var discountCard: String? { get }
+    var checkInTime: Date { get }
+    var parkedTime: Int { get }
 }
 
-struct Vehicle: Parkable {
-  let plate: String
-  let type: VehicleType
-  let checkInTime = Date()
-  var discountCard: String?
-  var parkingDuration: Int {
-    Calendar.current.dateComponents([.minute], from: checkInTime, to: Date()).minute ?? 0
-  }
-  
-  static func ==(lhs: Vehicle, rhs: Vehicle) -> Bool {
-    lhs.plate == rhs.plate
-  }
+struct Parking {
+    var vehicles: Set<Vehicle> //avoids duplicated vehicles
+    var totals: (vehiclesCount: Int, earnings: Int)
+}
+
+struct Vehicle: Parkable, Hashable {
+    let plate: String
+    let type: VehicleType
+    let checkInTime = Date()
+    var discountCard: String?
+    var parkedTime: Int {
+        Calendar.current.dateComponents([.minute], from: checkInTime, to: Date()).minute ?? 0
+    } //computed property
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(plate)
+    }
+    
+    static func ==(lhs: Vehicle, rhs: Vehicle) -> Bool {
+        return lhs.plate == rhs.plate
+    }
 }
 
 enum VehicleType {
-  case car
-  case miniBus
-  case bus
-  case motorcycle
-  
-  var tarifa: Int {
-    switch self {
-    case .car: return 20
-    case .motorcycle: return 15
-    case .miniBus: return 25
-    case .bus: return 30
+    case car
+    case miniBus
+    case bus
+    case motorcycle
+    
+    var tarifa: Int {
+        switch self {
+        case .car: return 20
+        case .motorcycle: return 15
+        case .miniBus: return 25
+        case .bus: return 30
+        }
     }
-  }
-}
-
-protocol Parkable: Hashable {
-  var plate: String { get }
-  var type: VehicleType { get }
-  var discountCard: String? { get }
-  var hasDiscountCard: Bool { get }
-  var checkInTime: Date { get }
-  var parkingDuration: Int { get }
 }
 
 extension Parkable {
-  
-  var hasDiscountCard: Bool { discountCard != nil }
+    
+    var hasDiscountCard: Bool { discountCard != nil }
 }
 
 extension Parkable {
-  
-  func hash(into hasher: inout Hasher) {
-    hasher.combine(plate)
-  }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(plate)
+    }
 }
 
